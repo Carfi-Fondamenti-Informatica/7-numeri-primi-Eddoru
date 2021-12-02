9ed44fabb2efa9785f4730ef40b846f6059c445d
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-12-02 06:15:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+void numeroprimo(int a, int i, int&conta){
+
+    int div=0;
+    i++;
+    if(a>1 and a>i){
+        div= a%i;
+        if(div==0) {
+            conta++;
+        }else{
+            numeroprimo(a,i,conta);
+
+        }
+    }
+}
