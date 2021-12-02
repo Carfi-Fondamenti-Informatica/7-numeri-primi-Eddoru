9e670e2bb803c43557df56a1b1ad66959e2e116b
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-12-02 06:14:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    
+    int num=0, conta=0;
+    cin>>num;
+    numeroprimo(num,1,conta);
+    if (conta==0) {
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+        
+    }
+
+
+    return 0;
 }
